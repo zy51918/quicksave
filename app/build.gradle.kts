10ea06f84e21c62671a33d6b9eb2208134b84c3e
zy51918 <38063145+zy51918@users.noreply.github.com>
2026-04-23 15:54:45 +0800

    build: add sh.calvin.reorderable dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.reorderable)
     testImplementation(libs.junit)
     testImplementation(libs.mockito.core)
     testImplementation(libs.mockito.kotlin)
